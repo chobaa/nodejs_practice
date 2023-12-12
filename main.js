ba344ea06d69e535f50f8d96c894fb9284a2deb0
chobaa <81730406+chobaa@users.noreply.github.com>
2023-12-12 14:07:42 +0900

    2023-12-12
    App 제작 - 모듈의 활용

--- main.js
+++ main.js
@@ -2,34 +2,9 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
+var template = require('./lib/template.js');
 const { describe } = require('node:test');
 const path = require('path');
-
-function templateHTML(title, list, body, control){
-  return  `<!doctype html>
-  <html>
-  <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-  </head>
-  <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    ${control}
-    ${body}
-  </body>
-  </html>
-  `;
-}
-
-function templateList(filelist){
-  var list = `<ul>`;
-  for(var i=0; i<filelist.length; i++){
-    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-  }
-  list += `</ul>`;
-  return list;
-}
 
 var app = http.createServer(function(request,response){
 var _url = request.url;
@@ -41,17 +16,25 @@ var title = queryData.id;
           fs.readdir('./data',(error,filelist) =>{
           title = 'Welcome';
           var description = 'Hello, Node.js';
+          
+          /*
           var list = templateList(filelist);
-          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+          var template = template.html(title,list,`<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
           response.writeHead(200);
           response.end(template);
+          */
+
+          var list = template.list(filelist);
+          var html = template.html(title,list,`<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+          response.writeHead(200);
+          response.end(html);
         })
       }
     else{
       fs.readdir('./data',(error,filelist) =>{
-          var list = templateList(filelist);
+          var list = template.list(filelist);
           fs.readFile(`./data/${queryData.id}`,'utf-8',(err,description) => {
-          var template = templateHTML(title,list,`<h2>${title}</h2>${description}`, 
+          var html = template.html(title,list,`<h2>${title}</h2>${description}`, 
           `<a href="/create">create</a>
           <a href="/update?id=${title}">update</a>
           <form action="delete_process" method="post">
@@ -59,27 +42,26 @@ var title = queryData.id;
           <input type="submit" value="delete">
           </form>`);
           response.writeHead(200);
-          response.end(template);
+          response.end(html);
         })
       })
     }
   }
   else if(pathname=="/create"){
     if(queryData.id == undefined){
-      fs.readdir('/data',(error,filelist) =>{
+      fs.readdir('./data',(error,filelist) =>{
         title = 'Web - create';
-        var description = 'Hello, Node.js';
-        var list = templateList(filelist);
-        var template = (templateHTML(title,list,`
-        <form action="/create_process"method="post">
+        var list = template.list(filelist);
+        var html = template.html(title,list,`
+        <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <textarea name="description" placeholder="description"></textarea>
         <input type="submit">
         </form>`,
         ``
-        ));
+        );
         response.writeHead(200);
-        response.end(template);
+        response.end(html);
       })
     }
   }
@@ -100,9 +82,9 @@ var title = queryData.id;
   }
   else if(pathname == "/update"){
     fs.readdir('./data',(error,filelist) =>{
-      var list = templateList(filelist);
+      var list = template.list(filelist);
       fs.readFile(`./data/${queryData.id}`,'utf-8',(err,description) => {
-      var template = templateHTML(title,list,`
+      var html = template.html(title,list,`
       <form action="/update_process" method="post">
       <input type="hidden" name="id" value="${title}">
         <p><input type="text" name="title" placeholder="title", value="${title}"></p>
@@ -111,7 +93,7 @@ var title = queryData.id;
         </form>
       `,  `<a href="/create">create</a> <a href="/update_process">update</a>`);
       response.writeHead(200);
-      response.end(template);
+      response.end(html);
     })
   })
   }
@@ -149,7 +131,7 @@ var title = queryData.id;
   }
   else{
     response.writeHead(404);
-    response.end(template);
+    response.end(html);
   }
 });
 app.listen(3000);
---
+++ syntax/object.js
@@ -0,0 +1,14 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); // k8805
+
+var roles = {
+    'programmer' : 'egoing',
+    'designer' : 'k8805',
+    'manager' : 'hoya'
+};
+
+console.log(roles.designer);  // k8805
+
+for(var name in roles){
+    console.log('keys =>' , name, 'values =>' , roles[name]);
+}
